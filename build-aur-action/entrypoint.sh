ac85f07167eb0de76a962c413d7c2d2556085329
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2021-11-30 10:58:29 +0800

    Add files via upload

---
+++ build-aur-action/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+useradd builder -m
+echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+chmod -R a+rw .
+
+pacman-key --init
+pacman -Sy --noconfirm &&
+echo "$INPUT_PRIVATE_SSH_KEY" >> ~./ssh/id_rsa
+chmod 600 ~./ssh/id_rsa
+
+echo '::group::Configuring git'
+git config --global user.name "$INPUT_GIT_USERNAME"
+git config --global user.email "$INPUT_GIT_EMAIL"
+echo '::endgroup::'
+
+git clone "ssh://aur@aur.archlinux.org/${INPUT_PKGNAME}.git"
+cd ${INPUT_PKGNAME}
+
+ver=$(curl -s https://api.github.com/repos/koreader/koreader/releases/latest | jq '.tag_name'|tr -d 'v"')
+sed -i "s/pkgver=.*/pkgver=$ver/" PKGBUILD
+updpkgsums
+makepkg --printsrcinfo > .SRCINFO
+rm koreader-$ver-amd64.deb
+
+git add .
+git commit -m $(date +"%Y-%m-%d_%T")
+git push
+echo OK
