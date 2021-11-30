a298605e57b0eb3c67b5ca352c6c3a57d58b98db
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2021-11-30 11:58:54 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -7,30 +7,12 @@ chmod -R a+rw .
 pacman-key --init
 pacman -Sy --noconfirm &&
 
-mkdir ~/.ssh
-
-echo "$INPUT_PRIVATE_SSH_KEY" >> ~/.ssh/id_rsa
-echo "$INPUT_KNOWN_HOSTS" >> ~/.ssh/known_hosts
-chmod 600 ~/.ssh/*
-
-echo '::group::Configuring git'
-git config --global user.name "$INPUT_GIT_USERNAME"
-git config --global user.email "$INPUT_GIT_EMAIL"
-echo '::endgroup::'
-
-cat ~/.ssh/id_rsa
-
-
-git clone "ssh://aur@aur.archlinux.org/${INPUT_PKGNAME}.git"
 cd ${INPUT_PKGNAME}
 
 ver=$(curl -s https://api.github.com/repos/koreader/koreader/releases/latest | jq '.tag_name'|tr -d 'v"')
 sed -i "s/pkgver=.*/pkgver=$ver/" PKGBUILD
 updpkgsums
-makepkg --printsrcinfo > .SRCINFO
+
 rm koreader-$ver-amd64.deb
 
-git add .
-git commit -m $(date +"%Y-%m-%d_%T")
-git push
 echo OK
