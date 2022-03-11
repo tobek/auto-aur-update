e0ed8b5886274c468960a177cbf21b8b6a53ab1c
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2022-03-11 20:38:31 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -9,18 +9,18 @@ pacman -Sy --noconfirm &&
 
 cd koreader-bin
 
-ver=$(curl -s https://api.github.com/repos/koreader/koreader/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/pkgver=.*/pkgver=$ver/" PKGBUILD
+ver_ko=$(curl -s https://api.github.com/repos/koreader/koreader/releases/latest | jq '.tag_name'|tr -d 'v"')
+sed -i "s/pkgver=.*/pkgver=$ver_ko/" PKGBUILD
 sudo -u builder  updpkgsums
 
 rm koreader-$ver-amd64.deb
 
 cd icalingua-plus-plus
 
-ver=$(curl -s https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/pkgver=.*/pkgver=$ver/" PKGBUILD
+ver_ica=$(curl -s https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest | jq '.tag_name'|tr -d 'v"')
+sed -i "s/pkgver=.*/pkgver=$ver_ica/" PKGBUILD
 sudo -u builder  updpkgsums
 
-rm app-i686-$ver.asar app-aarch64-$ver.asar app-x86_64-$ver.asar 
+rm app-i686-$ver_ica.asar app-aarch64-$ver_ica.asar app-x86_64-$ver_ica.asar 
 
 echo OK
