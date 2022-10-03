986bb12a1d7e66113342d01945fc00119efc97a2
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2022-10-03 12:37:07 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -24,4 +24,13 @@ cd -
 
 # rm app-i686-$ver_ica.asar app-aarch64-$ver_ica.asar app-x86_64-$ver_ica.asar 
 # cd -
+cd listen1
+
+ver_lis=$(curl -s https://api.github.com/repos/listen1/listen1_desktop/releases/latest | jq '.tag_name'|tr -d 'v"')
+sed -i "s/pkgver=.*/pkgver=$ver_lis/" PKGBUILD
+sudo -u builder  updpkgsums
+
+rm listen1_$ver_lis_linux_amd64.deb
+cd -
+
 echo OK
