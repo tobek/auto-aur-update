8264d52cbe22a0c6ec0e729c6c4c5f9fce770029
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2022-12-11 20:15:16 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -49,7 +49,7 @@ cd -
 
 cd sabaki-electron
 
-ver_cfw=$(curl -s https://api.github.com/repos/SabakiHQ/Sabaki/releases/latest | jq '.tag_name'|tr -d 'v"')
+ver_sbk=$(curl -s https://api.github.com/repos/SabakiHQ/Sabaki/releases/latest | jq '.tag_name'|tr -d 'v"')
 sed -i "s/pkgver=.*/pkgver=$ver_sbk/" PKGBUILD
 sudo -u builder  updpkgsums
 
