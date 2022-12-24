0e8187481ebd99f447f804a5fb5ff7498319f8f3
zzy-ac <74285757+zzy-ac@users.noreply.github.com>
2022-12-25 00:55:18 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -16,14 +16,14 @@ sudo -u builder  updpkgsums
 rm koreader-$ver_ko-amd64.deb
 cd -
 
-cd icalingua++
+#cd icalingua++
 
-ver_ica=$(curl -s https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest | jq '.tag_name'|tr -d 'v"')
-sed -i "s/pkgver=.*/pkgver=$ver_ica/" PKGBUILD
-sudo -u builder  updpkgsums
+#ver_ica=$(curl -s https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest | jq '.tag_name'|tr -d 'v"')
+#sed -i "s/pkgver=.*/pkgver=$ver_ica/" PKGBUILD
+#sudo -u builder  updpkgsums
 
-rm app-aarch64-$ver_ica.asar app-x86_64-$ver_ica.asar 
-cd -
+#rm app-aarch64-$ver_ica.asar app-x86_64-$ver_ica.asar 
+#cd -
 
 
 
