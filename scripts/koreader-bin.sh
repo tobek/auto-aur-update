603f71d8a6b3b11e7fb7e104795b3ed10c1fb3a6
zzy-ac <zzy-ac@qq.com>
2023-01-16 14:03:40 +0800

    update koreader-bin.sh

--- scripts/koreader-bin.sh
+++ scripts/koreader-bin.sh
@@ -4,5 +4,5 @@ ver_ko=$(curl -s https://api.github.com/repos/koreader/koreader/releases/latest 
 sed -i "s/pkgver=.*/pkgver=$ver_ko/" PKGBUILD
 sudo -u builder  updpkgsums
 
-rm koreader-v${ver_ko}-amd64.deb
+rm koreader-${ver_ko}-amd64.deb
 cd -
